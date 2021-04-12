c5622cbd82ec378e7e0d5969f20b4feb4e1980ba
gigi <lpgpirisi@gmail.com>
2021-04-12 14:02:21 +0200

    ciao

--- primary/tsne.py
+++ primary/tsne.py
@@ -498,7 +498,7 @@ def remove_outliers_lof(data, y):
     return X,y
 
 
-def mad_based_outlier(points, thresh=3.5):
+def mad_based_outlier(points, thresh=6):
     if len(points.shape) == 1:
         points = points[:,None]
     median = np.median(points, axis=0)
