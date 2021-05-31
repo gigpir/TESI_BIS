a6d99261699aa7fb3a1c5830d4d5c6317c9de782
gigi <lpgpirisi@gmail.com>
2021-05-31 15:04:34 +0200

    min fix

--- 4_SCRIPTS/merge_chunks.py
+++ 4_SCRIPTS/merge_chunks.py
@@ -5,6 +5,14 @@ sys.path.insert(1, '/home/crottondi/PIRISI_TESI/TESI_BIS/')
 import argparse
 from primary.data_io import save_data, load_data
 
+def getCurrentMemoryUsage():
+    ''' Memory usage in kB '''
+
+    with open('/proc/self/status') as f:
+        memusage = f.read().split('VmRSS:')[1].split('\n')[0][:-3]
+
+    return int(memusage.strip())
+
 
 def main(args):
     n_chunks = args.n_chunks
@@ -21,10 +29,12 @@ def main(args):
 
         for k,v in chunk_out.items():
             dictionary[k]=v
+        del chunk_out
 
+        print('chunk ', str(i), 'Memory (GB) : ', getCurrentMemoryUsage()/(2**20))
     final_pathname = chunk_folder+'merged_OUT.pkl'
 
-    save_data(dictionary,filename=final_pathname)
+    save_data(dict=dictionary, filename=final_pathname)
 
 if __name__ == '__main__':
 
